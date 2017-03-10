bda8247daf0f9bbd60cbf028d2abbbd55b2db6e4
dykdykdyk <1825041608@qq.com>
2017-03-10 18:00:23 +0800

    first command

--- HelloWorld/helloworld.cpp
+++ HelloWorld/helloworld.cpp
@@ -9,14 +9,28 @@ main()
 //	scanf("&f",&r);
 //	s= PI*r*r;
 //	printf("s=%d",s);
-	char a=176,b=219;
-	printf("%c%c%c%c%c\n",b,a,a,a,b);
-	printf("%c%c%c%c%c\n",a,b,a,b,a);
-	printf("%c%c%c%c%c\n",a,a,b,a,a);
-	printf("%c%c%c%c%c\n",a,b,a,b,a);
-	printf("%c%c%c%c%c\n",b,a,a,a,b);
+	int a=97,b=98;
+	if(a>100)
+	printf("a=%c,b=%c\n",a,b);
+	printf("a=%d,b=%d\n",a,b);
+	double e, f, g;
+	short h =121;
+	double i;
+	char j;
+//	j=getchar();
+//	i=10000/3.0;
+//    printf("%f\n",i);
+//    printf("%c\n",j);
+//	putchar('\n');
+//     printf("%20.15f\n",1.0/3);
+//	scanf("e=%If,f=%If,g=%If",&e,&f,&g);
+//	printf("%c%c%c%c%c\n",b,a,a,a,b);
+//	printf("%c%c%c%c%c\n",a,b,a,b,a);
+//	printf("%c%c%c%c%c\n",a,a,b,a,a);
+//	printf("%c%c%c%c%c\n",a,b,a,b,a);
+//	printf("%c%c%c%c%c\n",b,a,a,a,b);
     int c;
-	scanf("%5d",&c);//ֻȡǰ5λ
+//	scanf("%5d",&c);//ֻȡǰ5λ
 	printf("%d",c);
 //	printf("%c",b,a,a,a,b);
 }
