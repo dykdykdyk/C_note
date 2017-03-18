fa8dff815299aa7175304548d863d74e8fef8f68
dykdykdyk <1825041608@qq.com>
2017-03-18 17:20:14 +0800

    first command

--- struct/struct.cpp
+++ struct/struct.cpp
@@ -2,17 +2,22 @@
 结构体的简单实用
 */
 #include<stdio.h>
-struct student
+struct Student
 {
 	long num;//学号
 	char name[20];//姓名
 	char sex;//性别
 	float score;//成绩
-}
+}stu1;
+struct Data
+{
+	char addr[30];
+     struct Student st;
+};
 stu1 ={200501,"LinLin",'F',80.5};
 main()
 {
-	student stu2;
+	Student stu2;
 	stu2 =stu1; //引用整个结构体变量
 	printf("Number =%d\n",stu2.num);//引用结构体成员
 	printf("Name =%s\n",stu2.name);//引用结构体成员
